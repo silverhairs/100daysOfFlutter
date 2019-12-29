a4f0390a31dd37bca9cab181391ff2dc2cdc75a8
callmeBoris <boriskayienzo@gmail.com>
2019-12-29 13:35:47 +0200

    day #3 done

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -1,32 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audio_cache.dart';
-import 'dart:math';
 
 void main() => runApp(XylophoneApp());
 
-class XylophoneApp extends StatefulWidget {
-  @override
-  _XylophoneAppState createState() => _XylophoneAppState();
-}
+class XylophoneApp extends StatelessWidget {
+  void playSound(int counter) {
+    final player = AudioCache();
+    player.play('note$counter.wav');
+  }
 
-class _XylophoneAppState extends State<XylophoneApp> {
-  int audios = 1;
+  Widget touch({Color color, int soundNumber}) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playSound(soundNumber);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              child: Text('Click Me'),
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note$audios.wav');
-                setState(() {
-                  audios = Random().nextInt(7)+1;
-                });
-              },
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              touch(color: Colors.red, soundNumber: 1),
+              touch(color: Colors.orange, soundNumber: 2),
+              touch(color: Colors.yellow, soundNumber: 3),
+              touch(color: Colors.green, soundNumber: 4),
+              touch(color: Colors.teal, soundNumber: 5),
+              touch(color: Colors.blue, soundNumber: 6),
+              touch(color: Colors.purple, soundNumber: 7),
+            ],
           ),
         ),
       ),
