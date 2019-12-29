97ca575818c77590cbcd51b22079a25c13fc78a2
callmeBoris <boriskayienzo@gmail.com>
2019-12-29 04:21:33 +0200

    added sounds

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+import 'dart:math';
 
 void main() => runApp(XylophoneApp());
 
-class XylophoneApp extends StatelessWidget {
+class XylophoneApp extends StatefulWidget {
+  @override
+  _XylophoneAppState createState() => _XylophoneAppState();
+}
+
+class _XylophoneAppState extends State<XylophoneApp> {
+  int audios = 1;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: FlatButton(
+              child: Text('Click Me'),
+              onPressed: () {
+                final player = AudioCache();
+                player.play('note$audios.wav');
+                setState(() {
+                  audios = Random().nextInt(7)+1;
+                });
+              },
+            ),
+          ),
         ),
       ),
     );
