d1f3fc0ebfafd4179528dde112f16cf5cf24fba6
callmeBoris <boriskayienzo@gmail.com>
2019-12-30 23:54:00 +0200

    day #4 challenge done

--- quizzler/lib/main.dart
+++ quizzler/lib/main.dart
@@ -1,121 +1,125 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-void main() => runApp(Quizzler());
+import './quizz_brain.dart';
 
-class Quizzler extends StatelessWidget {
+void main() => runApp(QuizzApp());
+
+var quizzBrain = QuizzBrain();
+
+class QuizzApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.grey.shade900,
-        body: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10.0),
-            child: QuizPage(),
-          ),
+      home: QuizzHome(),
+    );
+  }
+}
+
+class QuizzHome extends StatefulWidget {
+  @override
+  _QuizzHomeState createState() => _QuizzHomeState();
+}
+
+class _QuizzHomeState extends State<QuizzHome> {
+  List<Widget> scoreIcons = [];
+  bool correctAnswer;
+
+  void checkAnswer(bool answer) {
+    setState(() {
+      if ((answer) == quizzBrain.getQuestionAnswer) {
+        scoreIcons.add(Icon(Icons.check, color: Colors.green));
+      } else {
+        scoreIcons.add(Icon(Icons.close, color: Colors.red));
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Expanded(
+              flex: 4,
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Center(
+                  child: Text(
+                    quizzBrain.getQuestionText,
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.sourceSansPro(
+                      textStyle: TextStyle(
+                        fontSize: 22,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.all(10),
+                child: FlatButton(
+                  color: Colors.green,
+                  child: Text(
+                    'True',
+                    style: GoogleFonts.sourceSansPro(
+                      textStyle: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  onPressed: () {
+                    correctAnswer = true;
+                    checkAnswer(correctAnswer);
+                    quizzBrain.nextQuestion();
+                  },
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.all(10),
+                child: FlatButton(
+                  color: Colors.red,
+                  child: Text(
+                    'False',
+                    style: GoogleFonts.sourceSansPro(
+                      textStyle: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  onPressed: () {
+                    correctAnswer = false;
+                    checkAnswer(correctAnswer);
+                    quizzBrain.nextQuestion();
+                  },
+                ),
+              ),
+            ),
+            Expanded(
+              child: SafeArea(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: scoreIcons,
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
   }
 }
-
-class QuizPage extends StatefulWidget {
-  @override
-  _QuizPageState createState() => _QuizPageState();
-}
-
-class _QuizPageState extends State<QuizPage> {
-  List<Icon> _scoreKeeper = [];
-
-  List<String> _questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        Expanded(
-          flex: 5,
-          child: Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Center(
-              child: Text(
-                'This is where the question text will go.',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 25.0,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              textColor: Colors.white,
-              color: Colors.green,
-              child: Text(
-                'True',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
-                ),
-              ),
-              onPressed: () {
-                setState(() {
-                  _scoreKeeper.add(
-                    Icon(
-                      Icons.check,
-                      color: Colors.green,
-                    ),
-                  );
-                });
-              },
-            ),
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              color: Colors.red,
-              child: Text(
-                'False',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
-                ),
-              ),
-              onPressed: () {
-                setState(() {
-                  _scoreKeeper.add(
-                    Icon(
-                      Icons.close,
-                      color: Colors.red,
-                    ),
-                  );
-                });
-              },
-            ),
-          ),
-        ),
-        Row(
-          children: _scoreKeeper,
-        )
-      ],
-    );
-  }
-}
-
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
