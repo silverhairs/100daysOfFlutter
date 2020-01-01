c092264c0b46f123f26e8933b50fdde5a2288586
callmeBoris <boriskayienzo@gmail.com>
2020-01-02 00:01:01 +0200

    removed some repeted code

--- beat_wizard/lib/key.dart
+++ beat_wizard/lib/key.dart
@@ -7,40 +7,37 @@ class BeatKey extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(top: 100.0),
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: BeatButton(
+                  background: Colors.blue,
+                  beat: 'superBeatt.wav',
+                ),
+              ),
+              Expanded(
+                child: BeatButton(
+                  background: Colors.teal,
+                  beat: 'violin.wav',
+                ),
+              ),
+            ],
+          ),
+        ),
         Row(
           children: <Widget>[
             Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.purple,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                margin:
-                    EdgeInsets.only(top: 120, left: 20, right: 20, bottom: 20),
-                height: 120.0,
-                child: FlatButton(
-                  onPressed: () {
-                    AudioCache player = AudioCache();
-                    player.play('superBeatt.wav');
-                  },
-                ),
+              child: BeatButton(
+                background: Colors.yellow,
+                beat: 'pop_dance.wav',
               ),
             ),
             Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                margin:
-                    EdgeInsets.only(top: 120, left: 20, right: 20, bottom: 20),
-                height: 120.0,
-                child: FlatButton(
-                  onPressed: () {
-                    AudioCache player = AudioCache();
-                    player.play('triangle.wav');
-                  },
-                ),
+              child: BeatButton(
+                background: Colors.orange,
+                beat: 'bass.wav',
               ),
             ),
           ],
@@ -48,76 +45,48 @@ class BeatKey extends StatelessWidget {
         Row(
           children: <Widget>[
             Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.teal,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-                height: 120.0,
-                child: FlatButton(
-                  onPressed: () {
-                    AudioCache player = AudioCache();
-                    player.play('violin.wav');
-                  },
-                ),
+              child: BeatButton(
+                background: Colors.green,
+                beat: 'triangle.wav',
               ),
             ),
             Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.orange,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-                height: 120.0,
-                child: FlatButton(
-                  onPressed: () {
-                    AudioCache player = AudioCache();
-                    player.play('bass.wav');
-                  },
-                ),
-              ),
-            ),
-          ],
-        ),
-        Row(
-          children: <Widget>[
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.cyan,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-                height: 120.0,
-                child: FlatButton(
-                  onPressed: () {
-                    AudioCache player = AudioCache();
-                    player.play('pop_dance.wav');
-                  },
-                ),
-              ),
-            ),
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.yellow,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-                height: 120.0,
-                child: FlatButton(
-                  onPressed: () {
-                    AudioCache player = AudioCache();
-                    player.play('beat2.wav');
-                  },
-                ),
+              child: BeatButton(
+                background: Colors.red,
+                beat: 'beat2.wav',
               ),
             ),
           ],
         ),
       ],
+    );
+  }
+}
+
+class BeatButton extends StatelessWidget {
+  final Color background;
+  final String beat;
+
+  BeatButton({
+    @required this.background,
+    @required this.beat,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: background,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+      height: 120.0,
+      child: FlatButton(
+        onPressed: () {
+          AudioCache player = AudioCache();
+          player.play(beat);
+        },
+      ),
     );
   }
 }
