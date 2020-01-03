8c26389f395d94099dfea649f325cc3742e1b4f3
callmeBoris <boriskayienzo@gmail.com>
2020-01-03 19:45:50 +0200

    finished design for the  gender row

--- bmi_calculator/lib/homepage.dart
+++ bmi_calculator/lib/homepage.dart
@@ -1,4 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import './icon_content.dart';
+import './layout_card.dart';
+
+const double bottomContainerHeight = 80.0;
+const Color activeColor = Color(0xFF1D1E33);
+const Color inactiveColor = Color(0xFF111328);
+const Color bottomContainerColor = Color(0xFFEB1555);
+
+enum Gender { male, female }
 
 class Homepage extends StatefulWidget {
   @override
@@ -6,21 +17,97 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  Color maleCardColor = inactiveColor;
+  Color femaleCardColor = activeColor;
+
+  //0 = male, 1 = female
+  void _updateColor(Gender gender) {
+    if (gender == Gender.male) {
+      if (maleCardColor == activeColor) {
+        maleCardColor = inactiveColor;
+        femaleCardColor = activeColor;
+      } else {
+        maleCardColor = activeColor;
+        femaleCardColor = inactiveColor;
+      }
+    } else if (gender == Gender.female) {
+      if (femaleCardColor == activeColor) {
+        femaleCardColor = inactiveColor;
+        maleCardColor = activeColor;
+      } else {
+        femaleCardColor = activeColor;
+        maleCardColor = inactiveColor;
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('BMI CALTULATOR'),
       ),
-      body: Center(
-        child: Text(
-          'Something',
-        ),
-      ),
-      floatingActionButton: Theme(
-        data: ThemeData.light(),
-        child: FloatingActionButton(
-          child: Icon(Icons.add),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _updateColor(Gender.male);
+                        });
+                      },
+                      child: LayoutCard(
+                        background: maleCardColor,
+                        layoutCardChild: IconContent(
+                            label: 'MALE', icon: FontAwesomeIcons.mars),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _updateColor(Gender.female);
+                        });
+                      },
+                      child: LayoutCard(
+                        background: femaleCardColor,
+                        layoutCardChild: IconContent(
+                          label: 'FEMALE',
+                          icon: FontAwesomeIcons.venus,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: LayoutCard(background: activeColor),
+            ),
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: LayoutCard(background: activeColor),
+                  ),
+                  Expanded(
+                    child: LayoutCard(background: activeColor),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              color: bottomContainerColor,
+              margin: EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: bottomContainerHeight,
+            )
+          ],
         ),
       ),
     );
