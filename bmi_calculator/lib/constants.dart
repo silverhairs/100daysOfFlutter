17709eba67c26570ee8c31f8da6956618936aee9
callmeBoris <boriskayienzo@gmail.com>
2020-01-05 03:35:58 +0200

    splitted constants from all widgets

---
+++ bmi_calculator/lib/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const double kBottomContainerHeight = 80.0;
+const Color kActiveColor = Color(0xFF1D1E33);
+const Color kInactiveColor = Color(0xFF111328);
+const Color kBottomContainerColor = Color(0xFFEB1555);
+
+const TextStyle kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xFF8D8E98),
+);
+
+const TextStyle kNumbersTextStyle = TextStyle(
+  fontSize: 50,
+  fontWeight: FontWeight.w900,
+);
