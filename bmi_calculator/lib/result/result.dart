a14b38bf9482e620be5352bfc9b30a9b9ca7d16a
callmeBoris <boriskayienzo@gmail.com>
2020-01-13 04:24:36 +0200

    starting results page

---
+++ bmi_calculator/lib/result/result.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class Result extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Result'),),
+    );
+  }
+}
